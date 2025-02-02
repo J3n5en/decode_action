59e3e8172acc285bf8d6859ae3e7ce03390e6601
action <action@github.com>
2025-02-02 07:40:53 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,38 +1,21 @@
-//Sun Feb 02 2025 07:37:05 GMT+0000 (Coordinated Universal Time)
+//Sun Feb 02 2025 07:40:53 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 var encode_version = "jsjiami.com.v5";
-function strencode(_0x67dc43, _0x4a4e2c, _0x4b0d50) {
-  l = _0x4b0d50.substr(-1);
-  if (l == 2) {
-    t = _0x67dc43;
-    _0x67dc43 = _0x4a4e2c;
-    _0x4a4e2c = t;
-  }
-  _0x67dc43 = atob(_0x67dc43);
-  len = _0x4a4e2c.length;
-  code = "";
-  for (i = 0; i < _0x67dc43.length; i++) {
-    k = i % len;
-    code += String.fromCharCode(_0x67dc43.charCodeAt(i) ^ _0x4a4e2c.charCodeAt(k));
-  }
-  return atob(code);
+function strencode2(_0x4f0d7a) {
+  return unescape(_0x4f0d7a);
 }
-(function (_0x3982b5, _0x5ef47c, _0x2a610c) {
-  _0x2a610c = "al";
+(function (_0x17883e, _0x4a42d3, _0xe4080c) {
+  _0xe4080c = "al";
   try {
-    {
-      _0x2a610c += "ert";
-      _0x5ef47c = encode_version;
-      if (!(typeof _0x5ef47c !== "undefined" && _0x5ef47c === "jsjiami.com.v5")) {
-        {
-          _0x3982b5[_0x2a610c]("删除版本号，js会定期弹窗，还请支持我们的工作");
-        }
-      }
+    _0xe4080c += "ert";
+    _0x4a42d3 = encode_version;
+    if (!(typeof _0x4a42d3 !== "undefined" && _0x4a42d3 === "jsjiami.com.v5")) {
+      _0x17883e[_0xe4080c]("删除版本号，js会定期弹窗，还请支持我们的工作");
     }
-  } catch (_0x109b3f) {
+  } catch (_0x23e6c5) {
     {
-      _0x3982b5[_0x2a610c]("删除版本号，js会定期弹窗");
+      _0x17883e[_0xe4080c]("删除版本号，js会定期弹窗");
     }
   }
 })(window);
